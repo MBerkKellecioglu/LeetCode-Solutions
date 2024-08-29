84bf7cb23c0fdbab384b1db6ba00a908e46ad3dd
Berk Kellecioğlu <wellfedkiro@hotmail.com>
2024-08-29 14:00:04 +0300

    Update Solution1.cpp

--- Two Pointers/Medium/Remove Duplicates from Sorted Array II/Solution1.cpp
+++ Two Pointers/Medium/Remove Duplicates from Sorted Array II/Solution1.cpp
@@ -6,19 +6,23 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         
+        // slow and fast pointers //
         int s = 0, f = 0;
 
-        while(f < nums.size()){
-            if(nums[s] == nums[f]) f++;
+        while(f < nums.size()){ 
+            if(nums[s] == nums[f]) f++; // for finding the range of repeated numbers //
             else{
                 if(f - s > 2){
+                    // erasing numbers so that there is only 2 numbers left at most //
                     nums.erase(nums.begin() + s, nums.begin() + f - 2);
+                    // rearranging fast pointer since we deleted some elements //
                     f -= (f - s - 2);
                 }
                 s = f;
             }
         }
 
+        // erasing numbers again since when f pointer breaks the loop the last deletion has not been occurred //
         if(f - s > 2) nums.erase(nums.begin() + s, nums.begin() + f - 2);        
         
         return nums.size();
